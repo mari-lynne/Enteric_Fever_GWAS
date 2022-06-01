7eb67f3427cb37fe8de3caa244d80a8b3f6645cf
mari-lynne <mari.johnson@lmh.ox.ac.uk>
2022-06-01 21:53:00 +0100

    set vars for sex check scripts/generalised

--- Pre-Impute/Sex_Check.R
+++ Pre-Impute/Sex_Check.R
@@ -5,28 +5,38 @@ library(dplyr)
 library(data.table)
 library(stringr)
 library(tidylog)
+library(dplyr)
+library(tidyr)
+library(RColorBrewer)
 
-setwd("~/GWAS_22/new_gwas/T1T2/sex_check")
-data <- "T1T2.fam"
-covar <- "~/GWAS_22/new_gwas/meta/covar.txt"
+#VAST covar file, need new file with updated cat_IDs
+covar <- "~/GWAS_22/new_gwas/meta/final_metadata/all_covar.txt"
+
+#covar <- "~/GWAS_22/new_gwas/meta/covar.txt"
+prefix <- "VAST_PATCH" #Plink prefix of data
+
+setwd(paste("~/GWAS_22/new_gwas/",prefix,"/sex_check", sep=""))
 
 #Check covar file ####
-data <- fread(data) #this has IDs FID = IID hence how they got joined at somepoint
-covar <- fread(covar) #1 is male 2 is female
+data <- fread(paste(prefix,".fam", sep =""))
+
+#This has IDs FID = IID hence how they got joined at somepoint
+covar <- fread(covar)
+#1 is male 2 is female
 
 #Make update sex file ####
-#fam file has .cel extension in ID name
-sex <- select(covar,GENOTYPING_ID,Sex)
+
+sex <- select(covar,IID,Sex)
 colnames(sex) <- c("V2","SEX")
 
 #add in fam column 
 fam <- data[,c(1:2)]
 fam$V2 <- as.character(fam$V2)
 sex <- left_join(fam, sex, by = "V2") #matched 105 rows
-  sex<-unique(sex)
+sex<-unique(sex)
   
 #Reorer so is FID, IID, Sex (as requested by plink)
-sex <- sex[,c(1,2,4)]
+#sex <- sex[,c(1,2,4)]
 colnames(sex) <- c("FID", "IID", "Sex")
 
 #Recode sex
@@ -34,38 +44,35 @@ colnames(sex) <- c("FID", "IID", "Sex")
 sex$Sex <- ifelse(sex$Sex == "M", 1, ifelse(sex$Sex == "F", 2, NA))
 
 #duplicate samples, check properly later
-sex <- sex[-c(19,45),]
+#sex <- sex[-c(19,45),]
 
 sex <- drop_na(sex, Sex)
 
-write.table(sex, file = "~/GWAS_22/new_gwas/T1T2/sex_check/update_sex.txt", quote = F, row.names = F, col.names = T, sep = "\t")
+#write.table(sex, file = "~/GWAS_22/new_gwas/T1T2/sex_check/update_sex.txt", quote = F, row.names = F, col.names = T, sep = "\t")
 
-#Try with plink2 first
-#--update-sex expects a file with FIDs and IIDs in the first two columns, and sex information (1 or M = male, 2 or F = female, 0 = missing
+#Save update file
+write.table(sex, file = "update_sex.txt", quote = F, row.names = F, col.names = T, sep = "\t")
 
-
-#Then run sex-check with y counts
-
-hh <- fread("rsv_redo.hh", header = F)
-#Family ID, Within-family ID,Variant ID
-bim <- rename(bim, SNP = V2)
-hh <- rename(hh, SNP = V3)
-chr_check <- left_join(hh, bim, by = "SNP")
-table(chr_check$V1.y)
-#Chr 23 = X chromosome where all the hetezygous snps are, isn't this what we would expect?
-
+#Exlude xy dodgy snps in R
 
 #More file cleaning
 #Load SNP info
-bim <-fread("T1T2.bim")
+bim <-fread(paste(prefix,".bim", sep =""))
 
 #Filter SNPs in autosomal chromosomes")
-table(bim$V1)
+sum <- table(bim$V1) #weird xy category with 22415 snps
+# 17599  1320   712
+sum
 
-xy <- bim %>% filter(V1 == "23" | V1 == "24" | V1 == "25")
+#T1T2 P1Tyger
+#xy <- bim %>% filter(V1 == "23" | V1 == "24" | V1 == "25")
+
+#VAST
+xy <- bim %>% filter(V1 == "X" | V1 == "XY" | V1 == "Y")
 
 #Remove duplicates 
 duplicated_snps <- xy[duplicated(paste(xy$V1,xy$V4)),]
+
 #remove snps where A > T, C > G
 #PLINK can't tell what strand to merge/read these SNPs from
 atcg_snps <-  xy[((xy$V5 == "A") & (xy$V6 == "T")) |
@@ -75,25 +82,40 @@ atcg_snps <-  xy[((xy$V5 == "A") & (xy$V6 == "T")) |
 #Exclusion list
 exclude <- rbind(duplicated_snps, atcg_snps)
 exclude <- exclude$V2
-write.table(file="sex_check/exclude_snps_xy.txt", exclude, sep = "\t", quote=F, row.names = F, col.names = F)
+write.table(file="exclude_snps_xy.txt", exclude, sep = "\t", quote=F, row.names = F, col.names = F)
+
+#Run sex_check.sh
 
 
-#Results #####
-library(ggplot2)
+#Plot ####
+sexcheck <- fread(paste(prefix,"_redo.sexcheck", sep=""))
 
-sexcheck <- fread("T1T2_redo.sexcheck")
-ggplot(sexcheck, aes(x=F)) + geom_histogram(bins =25) + labs(title = "Plink Sex Check -hh", x = " \nX-Chr Homozygosity (F-statistic)") #600W, H=385
-#below 0.2 = Female, above 0.8 = male (looks right from the data)
+sexcheck$SNPSEX <- as.factor(sexcheck$SNPSEX)
+levels(sexcheck$SNPSEX) <- c("M","F")
 
-#Heterozygosity check #####################
-#145 sample mis-sexed seems like a lot!
-#136021 het. haploid genotypes present (see rsv_sex_clean.hh ); many commands treat these as missing.
+ggplot(sexcheck, aes(x=F, fill =SNPSEX)) + geom_histogram(bins=25, color="black") + labs(title = paste(prefix,"Sex Check"), x = " \nX-Chr Homozygosity (F-statistic)") +scale_fill_brewer(palette="Dark2")
+#Males have only one copy of snp therefore homozygous
+#Females may have two copies of an X SNP therefore are heterozygous
 
-hh <- fread("rsv_sex_clean.hh", header = F)
-#Family ID, Within-family ID,Variant ID
 
-bim <- rename(bim, SNP = V2)
-hh <- rename(hh, SNP = V3)
 
-chr_check <- left_join(hh, bim, by = "SNP")
-table(chr_check$V1.y)
+#Write fail list ####
+
+fail <- filter(sexcheck, STATUS == "PROBLEM", PEDSEX != "0")
+write.table(file = paste(prefix, "_failSex.txt", sep = ""),fail, sep = "\t", quote=F, row.names = F, col.names = F)
+
+ggplot(fail, aes(x=F, fill =SNPSEX)) + geom_histogram(color="black") + labs(title = paste(prefix,"Sex Check"), x = " \nX-Chr Homozygosity (F-statistic)") +scale_fill_brewer(palette="Dark2")
+
+
+#impute sex for missing phenotypes
+
+impute <-  filter(sexcheck, PEDSEX == "0")
+write.table(file = paste(prefix, "_impSex.txt", sep = ""),impute, sep = "\t", quote=F, row.names = F, col.names = F)
+
+
+
+
+
+
+
+
--- Pre-Impute/Sex_check.sh
+++ Pre-Impute/Sex_check.sh
@@ -1,86 +1,77 @@
 !#/bin/bash
+set -uex
+#set so doesn't close upon erroring out 
 
 #Aims:
 
 #Check sex of genotyped individuals in enteric studies
 
 #Data
-#~/GWAS_22/new_gwas/VAST_PATCH/VAST_PATCH2.bim
-#~/GWAS_22/new_gwas/T1T2/T1T2.bim
+#~/GWAS_22/new_gwas/VAST_PATCH/VAST_PATCH2.bim #renamed to VAST_PATCH in sexdir
 #~/GWAS_22/new_gwas/P1Tyger/P1Tyger.bim"
 
-#Make update sex file from covar file
-#See Sex_Check.R
+#See Sex_Check.R for making update sex files
+#CD to sex dir in terminal then run script as source ~/GWAS_22/Enteric_Fever_GWAS/Pre-Impute/Sex_check.sh
+
+
+data=VAST_PATCH
+sexdir=~/GWAS_22/new_gwas/VAST_PATCH/sex_check
+echo $sexdir
+
+cd $sexdir
 
 plink \
---bfile T1T2 \
+--bfile $sexdir/${data} \
 --update-sex update_sex.txt \
 --make-bed \
---out T1T2_sex
+--out $sexdir/${data}_sex
 
-#Error: Invalid centimorgan position on line 58721 of T1T2.bim #works with old plink
+#Error: Invalid centimorgan position on line 58721 of $sexdir/${data}.bim
+#Need to use plink 1.9
 
 plink \
---bfile T1T2_sex \
+--bfile $sexdir/${data}_sex \
 --chr 23-24 \
 --maf 0.01 \
 --geno 0.05 \
 --mind 0.05 \
 --hwe 0.000001 \
 --make-bed \
---out T1T2_sex_chr
+--out $sexdir/${data}_sex_chr
 
 #exclude list in r
 plink \
---bfile T1T2_sex_chr \
+--bfile $sexdir/${data}_sex_chr \
 --exclude exclude_snps_xy.txt \
 --make-bed \
---out T1T2_sex_qc
+--out $sexdir/${data}_sex_qc
 
-#Warning: 69485 het. haploid genotypes present (see T1T2_sex_qc.hh ); many
-#Total genotyping rate is 0.997583.
-#5804 variants and 318 people pass filters and QC.
-
+#Prune SNPs
 plink \
---bfile T1T2_sex_qc \
---split-x b37 no-fail\
---make-bed \
---out T1T2_sex_chr
-
-#43 chromosome codes changed, is that 43 variants?
-#hh genotypes stayed the same so not an autosome region problem
-
-plink \
---bfile T1T2_sex_qc \
+--bfile $sexdir/${data}_sex_qc \
 --set-hh-missing \
 --indep-pairphase 20000 2000 0.5 \
---out T1T2_sex_LD
+--out $sexdir/${data}_sex_LD
 
-#Warning: 66700 het. haploid genotypes present (see T1T2_sex_LD.hh )
-#5804 variants and 318 people pass filters and QC.
-#Pruned 2065 variants from chromosome 23, leaving 3696.
-#Pruned 14 variants from chromosome 25, leaving 29.
-#Pruning complete.  2079 of 5804 variants removed.
-
-
+#Exclude pruned SNPs
 plink \
---bfile T1T2_sex_qc \
---extract T1T2_sex_LD.prune.in \
+--bfile $sexdir/${data}_sex_qc \
+--extract $sexdir/${data}_sex_LD.prune.in \
 --make-bed \
---out T1T2_sex_clean
-#Warning: 42368 het. haploid genotypes present (see T1T2_sex_clean.hh ); many
-#3725 variants and 318 people pass filters and QC.
+--out $sexdir/${data}_sex_clean
 
 plink \
---bfile T1T2_sex_clean \
+--bfile $sexdir/${data}_sex_clean \
 --set-hh-missing \
 --make-bed \
---out T1T2_sex_hh
+--out $sexdir/${data}_sex_hh
 
-#65 mismatches with het-haplotypes removed
-#145 mismatches without
-
+#Add in ycount statistic
 plink \
---bfile T1T2_sex_hh \
+--bfile $sexdir/${data}_sex_hh \
 --check-sex ycount  \
---out T1T2_redo
+--out $sexdir/${data}_redo
+
+mkdir log
+
+mv *.log log
