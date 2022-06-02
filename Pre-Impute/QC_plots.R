ed4ae6deb8e49a1711dca56ba29b188a1fd74ca2
mari-lynne <mari.johnson@lmh.ox.ac.uk>
2022-06-02 14:07:05 +0100

    Plotting MAF post-impute

--- Pre-Impute/QC_plots.R
+++ Pre-Impute/QC_plots.R
@@ -42,6 +42,8 @@ library(patchwork)
 #895,0259 variants remaining after main filters.
 #((16894768 - 7944509)/16894768)*100 = 52% removed
 
+#Then merged
+
 #Plans ####
 
 #Pre-impute plots/summaries 
@@ -62,33 +64,12 @@ Need plots for MIND/Geno thresholds, IBD, sex_check
 
 # Summary:
 #Using Plink we ran an initial filtering step for snps of low quality. These included SNPS
+#Sex check 7 samples discordant information
+#Also able to impute sex of missing participants
 
 #2) Individual missingness SNP plot 
 
 
-
-
-
-#1) Plot MAF ####
-#Get frequency file
-#T1T2_remap.frq
-
-setwd('/home/mari/GWAS_22/new_gwas/QC')
-#Generate frequency file
-system("plink bfile P1T1 --freq")
-
-maf_freq <- read.table("all_enteric.frq", header =TRUE, as.is=T)
-
-
-
-
-
-setwd('/home/mari/GWAS_22/new_gwas/Post-imp/Merged/Assoc')
-#PLINKQC Set up
-indir<-"~/GWAS_22/new_gwas/Post-imp/Merged"
-qcdir<-"~/GWAS_22/new_gwas/Post-imp/Merged"
-name<-"VAST_concat"
-path2plink <- "/home/mari/bin/plink"
 
 
 
@@ -98,6 +79,70 @@ path2plink <- "/home/mari/bin/plink"
 
 
 #Post-impute plots ####
+setwd('/home/mari/GWAS_22/new_gwas/QC')
+
+#MAF ####
+#Generate frequency file
+system("plink --bfile VAST_imp_QC --freq --out VAST_PATCH")
+
+maf_freq <- read.table("VAST_PATCH.frq", header =TRUE, as.is=T)
+
+str(maf_freq)
+
+a <- maf_freq %>%
+  ggplot(aes(x = MAF, y = ..count..)) + 
+  geom_histogram(fill = "purple", colour = "grey") + geom_vline(xintercept = 0.05, linetype="dashed", 
+                                                                color = "blue", size=1) +ggtitle("Total imputed SNPs") +ylab("Count\n") + xlab("Minor allele frequency") +theme_bw() + scale_y_continuous(labels= fancy_scientific)
+
+b <- maf_freq %>% filter(MAF > 0.05) %>%
+  ggplot(aes(x = MAF, y = ..count..)) + 
+  geom_histogram(bins = 22, fill = "purple", colour = "grey", alpha =0.8) +ggtitle("Filtered SNPs, MAF <0.05") +ylab("Count") + xlab("Minor allele frequency") +theme_bw() + scale_y_continuous(labels= fancy_scientific)
+
+plot <- a + b + plot_annotation(tag_levels = "A")
+plot
+
+#A4 is 21cm W, 29 cm L (no margins)
+#tiff("imputed_maf.tiff", units="cm", width=17, height=14, res=300, compression = "lzw")
+#print(plot)
+#dev.off()
+
+
+#ggsave version
+plot <- a + b + plot_annotation(tag_levels = "A")
+plot
+ggsave(filename = "imputed_maf.tiff", plot = plot, units = "in", width = 7, height = 4)
+
+
+#Prioritise IBD and ancestry plots
+
+
+
+#Ancestry ####
+
+
+
+
+
+
+
+
+#makes the scientific notation using "AeB" explicitly write out Ax10^B
+fancy_scientific <- function(l) {
+  #Rename first 0
+  l <- gsub("0e\\+00", "0", l)
+  # turn in to character string in scientific notation
+  l <- format(l, scientific = TRUE)
+  # quote the part before the exponent to keep all the digits
+  l <- gsub("^(.*)e", "'\\1'e", l)
+  # turn the 'e+' into plotmath format
+  l <- gsub("e\\+", "%*%10^", l)
+  # return this as an expression
+  parse(text=l)
+}
+
+
+
+
 #Set up wd ##
 setwd('/home/mari/GWAS_22/new_gwas/Post-imp/Merged/Assoc')
 #PLINKQC Set up
@@ -105,25 +150,3 @@ indir<-"~/GWAS_22/new_gwas/Post-imp/Merged"
 qcdir<-"~/GWAS_22/new_gwas/Post-imp/Merged"
 name<-"VAST_concat"
 path2plink <- "/home/mari/bin/plink"
-
-
-maf_freq <- read.table("all_enteric.frq", header =TRUE, as.is=T)
-pdf("MAF_distribution.pdf")
-
-hist(maf_freq[,5],main = "MAF distribution", xlab = "MAF")
-
-str(maf_freq)
-
-b <- maf_freq %>% filter(MAF > 0.05) %>%
-  ggplot(aes(x = MAF, y = ..count..)) + 
-  geom_histogram(bins = 24, fill = "purple", colour = "grey") +ggtitle("Filtered SNPs") +ylab("Count\n") + xlab("Minor allele frequency") +theme_minimal()
-
-a <- maf_freq %>%
-  ggplot(aes(x = MAF, y = ..count..)) + 
-  geom_histogram(fill = "purple", colour = "grey") + geom_vline(xintercept = 0.05, linetype="dashed", 
-                                                                color = "blue", size=1) +ggtitle("Total Imputed SNPs") +ylab("Count\n") + xlab("Minor allele frequency") +theme_minimal()
-
-a
-
-a + b + plot_annotation(tag_levels = "A")
-
