7943ddb1bd71d138cf996d09d90b2c4b13c9a719
mari-lynne <mari.johnson@lmh.ox.ac.uk>
2022-06-07 00:03:40 +0100

    recoded live oral vaccines as separate vars

--- Post-Impute/meta_updating/covar_updating.R
+++ Post-Impute/meta_updating/covar_updating.R
@@ -1,7 +1,12 @@
-#Update Covar files
+#Update Covar files #Check Jun/22
 
 #Needs to be recoded as categorical I think 
-covar <- fread("all_covar.txt")
+#covar <- fread("all_covar.txt")
+
+check <- fread("~/GWAS_22/new_gwas/meta/final_metadata/all_covar.txt")
+covar <- fread("~/GWAS_22/new_gwas/meta/final_metadata/all_covar.txt")
+
+table(check$Vaccine) #adds up ok :)
 
 #Recode Sex
 #Recode Challene
@@ -16,10 +21,11 @@ covar <- fread("all_covar.txt")
 #Vi-TT= 4
 
 covar$Vaccine <- ifelse(covar$Vaccine == "None",0,
-                            ifelse(covar$Vaccine == "Ty21a",2,
+                            ifelse(covar$Vaccine == "Ty21a",1,
                                    ifelse(covar$Vaccine == "M01ZH09", 2,
                                           ifelse(covar$Vaccine == "Vi-PS", 3,
                                                  ifelse(covar$Vaccine == "Vi-TT", 4,NA)))))
+#Ty21 coded as 2 instead of 1 need to remake covar
 
 #Recode strain
 covar$Challenge <- as.factor(covar$Challenge)
@@ -37,8 +43,8 @@ covar$Sex <- ifelse(covar$Sex == "M", 1, ifelse(covar$Sex == "F", 2, NA))
 covar$Re_Challenge <- as.factor(covar$Re_Challenge)
 levels(covar$Re_Challenge)
 #N=1, PP=2, PT=3, TP=4, TT=5
-
 levels(covar$Re_Challenge) <- c("1", "2", "3", "4", "5")
+
 
 #Key: ####
 
@@ -46,3 +52,5 @@ levels(covar$Re_Challenge) <- c("1", "2", "3", "4", "5")
 #Sex M=1, F=2
 #Re-challenge N=1, PP=2, PT=3, TP=4, TT=5
 #Vaccine: NONE = 0, Ty21a = 1, M01ZH09= 2, Vi-PS = 3, Vi-TT= 4
+
+write.table(covar, file = "~/GWAS_22/new_gwas/meta/final_metadata/all_covar_june.txt", row.names = F, quote = F, col.names = T, sep = "\t")
