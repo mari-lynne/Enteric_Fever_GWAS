ec1e72abe5ae3e644f090c5bbf1e2dedf26196b5
mari-lynne <mari.johnson@lmh.ox.ac.uk>
2022-10-17 19:38:27 +0100

    Updated files, deleted old scripts

--- Post-Impute/QC/3.pca_qc.sh
+++ Post-Impute/QC/3.pca_qc.sh
@@ -4,7 +4,6 @@
 # source ~/GWAS_22/Enteric_GWAS/Post-Impute/QC/3.pca_qc.sh
 
 # Variables and Directories -----------------------------------------------------------
-
 name=typhoid
 ref=all_hg38
 
@@ -15,20 +14,19 @@ refdir=${qcdir}/ref
 mkdir ${dir}/pca
 cd ${qcdir}
 
-# Update fam file and extract pop data ---------------------------------------------
+# Update fam/psam file and extract pop data ---------------------------------------------
 
 # Earlier conversion of .psam to .fam lost pheno/pop data of 1KG participants
 # Eigenvec file has FID (0s) IID PC1-10
-# get ID and pop from original psam file 
+# Get ID and pop from original psam file 
 # psam file needs an updated FID column
 
 # Add header to fam file using sed
 # Paste fam file into psam to add the FAM column
 sed -i '1i FID\tIID\tPAT\tMAT\tSEX\tPHENOTYPE' ${refdir}/${ref}.fam 
-paste ${refdir}/${ref}.fam ${refdir}/${ref}.psam |\
-cut -f 1,7-12 > ${qcdir}/${ref}.psam
+paste ${refdir}/${ref}.fam ${refdir}/${ref}.psam \
+| cut -f 1,7-12 > ${qcdir}/${ref}.psam
 # Remove extra columns 
-
 awk '{print $1, $2, $6, $7, $8}' ${qcdir}/${ref}.psam > ${qcdir}/1kG.ID2Pop.txt
 
 # Merged study/ancestry PCA ---------------------------------------------------------------
