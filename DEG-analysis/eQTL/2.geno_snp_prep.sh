996421c71d165cf8c6b332c61d925755a5151a8d
mari-lynne <mari.johnson@lmh.ox.ac.uk>
2022-10-23 16:38:55 +0100

    Also used to make traw files for plotting

--- DEG-analysis/eQTL/2.geno_snp_prep.sh
+++ DEG-analysis/eQTL/2.geno_snp_prep.sh
@@ -1,41 +1,48 @@
 #!/bin/bash
-
 set -ux
 
 # Aims:
 # Gather snp and genotyping data and make meQTL files
 # run script as source ~/GWAS_22/Enteric_GWAS/DEG-analysis/eQTL/2.geno_snp_prep.sh
 plink_data=typhoid2.IBD
-plink_dir=~/GWAS_22/gwas_final/merge/typhoid/QC
+dir=~/GWAS_22/gwas_final/merge/typhoid
+plink_dir=${dir}/QC
 
-outdir=~/GWAS_22/gwas_final/eQTL # same as outdir in Rscript
-time_point=12h
-study=T1T2_
+#~/GWAS_22/gwas_final/merge/typhoid/assoc/nexus/adnob
+# data renamed to data
+data=diagnosis
+study=vast_
+outdir=${dir}/assoc/vast/${data} 
+# same as outdir in Rscript :: EQTL = ~/GWAS_22/gwas_final/eQTL
+ # Or adnob/antibody measure
 
 cd ${outdir}
+# Make snp keep file from gwas data (not needed for eqtl)
+cut -f3 tophits_${data}.txt > ${study}${data}_snp_keep.txt
 
 # Filter plink data for study individuals, and snps for eqtl analysis
 plink2 --bfile ${plink_dir}/${plink_data} \
---keep ${study}${time_point}_keep.txt \
---extract ${study}${time_point}_snp_keep.txt \
+--keep ${plink_dir}/${study}keep.txt \
+--extract ${study}${data}_snp_keep.txt \
 --make-bed \
---out ${study}${time_point}
+--out ${study}${data}
 
 #Recode SNPs as 1's and 0's
 # Transpose table so SNPs are rows, participants are columns
-plink --bfile ${study}${time_point} \
+plink --bfile ${study}${data} \
 --allow-no-sex \
 --recode oxford \
---out ${study}${time_point}
+--out ${study}${data}
 
-plink --data ${study}${time_point} \
+plink --data ${study}${data} \
 --allow-no-sex \
 --recode A-transpose \
---out ${study}${time_point}
+--out ${study}${data}
 
 #  CHR	SNP	(C)M	POS	COUNTED	ALT
 # Cut relevant data from new file
-cat ${study}${time_point}.traw | cut -f2,7- > ${study}${time_point}_geno.txt
-awk '{print $2, $1, $4}' ${study}${time_point}.traw > ${study}${time_point}_snp_loc.txt
+cat ${study}${data}.traw | cut -f2,7- > ${study}${data}_geno.txt
+awk '{print $2, $1, $4}' ${study}${data}.traw > ${study}${data}_snp_loc.txt
 
 rm *.nosex
+mv *log log
