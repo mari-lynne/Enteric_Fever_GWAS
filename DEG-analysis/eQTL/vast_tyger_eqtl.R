1e43f006c3d53acae7d66ec8beca8935b982b435
mari-lynne <mari.johnson@lmh.ox.ac.uk>
2022-10-26 21:57:55 +0100

    just vast analysis

--- DEG-analysis/eQTL/vast_tyger_eqtl.R
+++ DEG-analysis/eQTL/vast_tyger_eqtl.R
@@ -25,13 +25,14 @@ library(biomaRt)
 setwd("~/GWAS_22/gwas_final/eQTL")
 plot_dir <- c("~/GWAS_22/gwas_final/eQTL/plots") 
 time_point <- c("D0.12h")
-study <- c("/vast_tyg_")
+study <- c("/vast_")
 out_dir <- getwd()
 
 id_data <- c("~/GWAS_22/gwas_final/meta/geno_ids.csv")
-plink <- c("~/GWAS_22/gwas_final/merge/typhoid/QC/typhoid2.IBD.fam") #fam file
-assoc_data <- c("~/GWAS_22/gwas_final/merge/typhoid/assoc/typhoid_tophits.txt")
-covar_data <- c("~/GWAS_22/gwas_final/meta/typhoid_pcacovar.txt")
+plink <- c("~/GWAS_22/gwas_final/merge/typhoid/vast/vast.fam") #fam file
+assoc_data <- c("~/GWAS_22/gwas_final/merge/typhoid/vast/iga_tophits.txt")
+covar_data <- c("~/GWAS_22/gwas_final/meta/covar_vast.txt")
+# typhoid2.IBD, typhoid_pcacovar
 
 pheno_exprs <- read.csv(file = "~/RNA/oct/combat_vast_tyger.csv")
 pheno <- pheno_exprs[, !str_detect(colnames(pheno_exprs), "ENSG")]
@@ -45,8 +46,7 @@ pheno <- pheno_exprs[, !str_detect(colnames(pheno_exprs), "ENSG")]
 # 1) Filter time point
 pheno_exprs <-
   pheno_exprs %>%
-  filter(time_point == "D0.12h")
-
+  filter(time_point == "D0.12h", study_arm %in% c("ViTCV", "ViPS"))
 
 
 # 2) Filter for gwas samples ---------------------------------------------------
@@ -83,7 +83,6 @@ fam <- fread(plink)
 fam <- fam %>% dplyr::rename(FID = V1,
                              IID = V2)
 # Get FIDs
-
 keep_ids <- inner_join(keep, fam, by = "IID")
 keep <- inner_join(keep, fam, by = "IID") %>% dplyr::select(FID, IID)
 
@@ -94,7 +93,6 @@ write.table(keep,
             row.names = F,
             quote = F,
             col.names = T)
-
 
 # Filter for topsnps ----------------------------------------------------------
 
@@ -152,20 +150,26 @@ filtered_genes <- filtered_genes[(filtered_genes$chromosome_name %in% c(1:22)),]
 filtered_genes[filtered_genes == ""]<- NA
 filtered_genes <- na.omit(filtered_genes) %>% dplyr::select(-ensembl_gene_id) #rewrite table
 
+# filter na genes
+filtered_genes <- filter(filtered_genes, !is.na(hgnc_symbol)) 
+
+# Filter exprs
+exprs <- filter(exprs, exprs$hgnc_symbol %in% filtered_genes$hgnc_symbol)
+
+
 write.table(filtered_genes, file = paste0(out_dir,study,time_point,"_gene_loc.txt"),
-            sep = "\t", quote = F, col.names = T, row.names = F)
+            sep = "\t", quote = F, row.names = F)
 
 write.table(exprs, file = paste0(out_dir,study,time_point,"_exprs.txt"),
-            sep = "\t", quote = F, col.names = T, row.names = F)
+            sep = "\t", quote = F,row.names = F)
 
 # Covar table -----------------------------------------------------------------
 
-covar <- fread("~/GWAS_22/gwas_final/meta/typhoid_pcacovar.txt")
+covar <- fread(covar_data)
 # filter for study participants
 covar <- covar %>% filter(IID %in% keep$IID)
 # 93-120, VAST-3104, 8280 (Vi-PS?, but control in pheno file)
 covar <- dplyr::select(covar, IID, sex, age, chall_vax)
-
 
 # recode chall_vax to numeric
 covar$chall_vax <- as.factor(covar$chall_vax)
@@ -215,3 +219,130 @@ write.table(covar, file = paste0(out_dir,study,time_point,"_covar.txt"),
 
 write.table(exprs, file = paste0(out_dir,study,time_point,"_exprs.txt"),
             sep = "\t", quote = F, col.names = T, row.names = F)
+
+# Run eqTL ---------------------------------------------------------------------
+
+library(MatrixEQTL)
+
+var <- c("iga")
+out_dir = getwd()
+SNP_file_name = paste0(out_dir, study, time_point, "_geno.txt");
+snps_location_file_name = paste0(out_dir, study, time_point, "_snp_loc.txt");
+
+# Gene expression file name
+expression_file_name = paste0(out_dir, study, time_point, "_exprs.txt");
+gene_location_file_name = paste0(out_dir, study, time_point, "_gene_loc.txt");
+
+# Covariates file name
+# Set to character() if there are no covariates
+covariates_file_name = paste0(out_dir, study, time_point,"_covar.txt");
+
+# Output file name
+output_file_name_cis = tempfile();
+output_file_name_tra = tempfile();
+
+## Thresholds -------------------------------------------------------------------
+
+# Only associations significant at this level will be saved
+pvOutputThreshold_cis = 1e-2;
+pvOutputThreshold_tra = 1e-4;
+
+# Error covariance matrix
+# Set to numeric() for identity.
+errorCovariance = numeric();
+# Distance for local gene-SNP pairs
+cisDist = 1e6; #1MB
+
+# Load data --------------------------------------------------------------------
+
+## Load genotype data
+
+snps = SlicedData$new();
+snps$fileDelimiter = "\t";      # Space delim
+snps$fileOmitCharacters = "NA"; # denote missing values;
+snps$fileSkipRows = 1;          # one row of column labels
+snps$fileSkipColumns = 1;       # one column of row labels
+snps$fileSliceSize = 2000;      # read file in slices of 2,000 rows
+snps$LoadFile(SNP_file_name);
+
+## Load gene expression data
+
+gene = SlicedData$new();
+gene$fileDelimiter = "\t";      # Space delim
+gene$fileOmitCharacters = "NA"; # denote missing values;
+gene$fileSkipRows = 1;          # one row of column labels
+gene$fileSkipColumns = 1;       # one column of row labels
+gene$fileSliceSize = 2000;      # read file in slices of 2,000 rows
+gene$LoadFile(expression_file_name);
+
+## Load covariates
+cvrt = SlicedData$new();
+cvrt$fileDelimiter = "\t";      # Space delim
+cvrt$fileOmitCharacters = "NA"; # denote missing values;
+cvrt$fileSkipRows = 1;          # one row of column labels
+cvrt$fileSkipColumns = 1;       # one column of row labels
+if(length(covariates_file_name)>0) {
+  cvrt$LoadFile(covariates_file_name);
+}
+
+## Load location files
+snpspos = read.table(snps_location_file_name, header = TRUE, stringsAsFactors = FALSE);
+genepos = read.table(gene_location_file_name, header = TRUE, stringsAsFactors = FALSE);
+
+# Run mEQTL analysis ------------------------------------------------------------
+
+# Define model
+useModel = modelLINEAR; 
+
+me = Matrix_eQTL_main(
+  snps = snps, 
+  gene = gene, 
+  cvrt = cvrt,
+  output_file_name      = output_file_name_tra,
+  pvOutputThreshold     = pvOutputThreshold_tra,
+  useModel = useModel, 
+  errorCovariance = errorCovariance, 
+  verbose = TRUE, 
+  output_file_name.cis  = output_file_name_cis,
+  pvOutputThreshold.cis = pvOutputThreshold_cis,
+  snpspos = snpspos, 
+  genepos = genepos,
+  cisDist = cisDist,
+  pvalue.hist = TRUE,
+  min.pv.by.genesnp = FALSE,
+  noFDRsaveMemory = FALSE);
+
+unlink(output_file_name_tra);
+unlink(output_file_name_cis);
+
+# Results: --------------------------------------------------------------------
+
+cat('Analysis done in: ', me$time.in.sec, ' seconds', '\n');
+
+trans <- (me$trans$eqtls)
+cis <- (me$cis$eqtls)
+
+bonferroni <- (0.05/me$trans$ntests)
+
+#trans <- filter(trans, FDR <=0.05)
+#cis <- filter(cis, FDR <=0.05)
+
+## Merge gwas data ------------------------------------------------------------
+
+trans$eqtl <- rep("trans", nrow(trans))
+cis$eqtl <- rep("cis", nrow(cis))
+
+eqtls <-
+  bind_rows(cis, trans) 
+
+## Merge assoc data ####
+assoc <- fread(assoc_data)
+
+eqtls <-
+  eqtls %>%
+  rename(SNP = snps) %>%
+  left_join(assoc, by = "SNP")
+
+write.csv(eqtls,
+          file = paste0(out_dir, study, time_point, var,"_eqtl.csv"),
+          row.names = FALSE)
